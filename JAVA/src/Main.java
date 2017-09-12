ebe6ada4638dc929b5ca26273963849944dc4174
KennyNguyen88 <poscoictvn@Poscos-MacBook-Pro.local>
2017-09-12 09:56:10 +0700

    JAVA - #001 Print to Console
    Init
    #001 - Print to Console

---
+++ JAVA/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        //#001 Print to Console
+        PrintToConsole.demo();
+    }
+}
---
+++ JAVA/src/PrintToConsole.java
@@ -0,0 +1,7 @@
+public class PrintToConsole {
+    public static void demo(){
+        System.out.print("Something to print !");
+        System.out.println("Something to print with new line !");
+    }
+
+}
